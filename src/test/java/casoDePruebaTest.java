bea15923df595aedcb43fec2793faaafcc5439d5
unknown <Kevin.pena@tsoftglobal.com>
2021-06-04 14:39:42 -0300

    Se agregaron Test-Case al proyecto, y dependendencias

---
+++ src/test/java/casoDePruebaTest.java
@@ -0,0 +1,44 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+    public class casoDePruebaTest {
+        WebDriver driver;
+
+        public casoDePruebaTest() {
+
+        }
+
+        public String casoSelenium1() {
+            String resultado = "";
+
+            resultado = driver.findElement(By.xpath("/html/body/h1")).getText();
+
+            return resultado;
+        }
+
+        @Before
+        public void setup() throws InterruptedException {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("http://192.168.102.129:8082/Prueba_2020");
+            Thread.sleep(3000);
+        }
+
+        @Test
+        public void casoSelenium1Test() {
+            Assert.assertEquals("Hola Mundo Tsoft 2021", casoSelenium1());
+            System.out.println("El test avanzo con exito");
+        }
+
+        @After
+        public void after() {
+            driver.quit();
+        }
+    }
+
